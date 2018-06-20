1e919462ebb1dac16381c163d976fd79db0b9f07
Armannpetur <armanna16@ru.is>
2018-06-20 14:14:30 +0000

    front page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,8 @@ import { Grid, Row, Col, Image } from 'react-bootstrap';
 import './Home.css';
 import logo from './../Utils/assets/a+r.jpg';
 import kirkja from './../Utils/assets/kirkja.jpg';
+import hjon from './../Utils/assets/profile.jpg';
+import salur from './../Utils/assetshlidarbaer.jpg';
 
 export default class Home extends Component {
     render() {
@@ -16,12 +18,12 @@ export default class Home extends Component {
                         <p>Athöfnin verður í Akureyrarkirkju kl 15:30</p>
                     </Col>
                     <Col xs={12} sm={4} className="person-wrapper">
-                        <Image src="assets/profile.jpg" circle className="profile-pic-center" />
+                        <img src={hjon} alt="hjon" className="profile-pic-center" />
                         <h3>Brúðhjón</h3>
                         <p>Við erum að fara gifta okkur þann 4.ágúst 2018.</p>
                     </Col>
                     <Col xs={12} sm={4} className="person-wrapper">
-                        <Image src="assets/hlidarbaer.jpg" circle className="profile-pic" />
+                        <img src={salur} className="profile-pic" />
                         <h3>Veislusalur</h3>
                         <p>Veislan er haldin í Hlíðarbæ strax í kjölfar athafnar</p>
                     </Col>
